580409143876b797f085e29aeecf8c81a141df6a
Tornikesamkharadze <tornikesamkharadzee@gmail.com>
2026-03-02 13:00:29 +0400

    upw churckkhela

--- components/MasterplanSection.js
+++ components/MasterplanSection.js
@@ -19,7 +19,7 @@ export default function MasterplanSection() {
         whileHover={{ scale: 1.02 }}
         src="/akr.webp"
         alt="Masterplan"
-        className="w-full h-screen object-containe"
+        className="w-full h-screen object-fill"
       />
     </section>
   );
